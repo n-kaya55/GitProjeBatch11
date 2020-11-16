637ea9d982f34cd7e9797e8fff9a9801cb764880
n-kaya55 <nesibe.kaya104@gmail.com>
2020-11-17 00:01:30 +0300

    ikinci commit

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -1,9 +1,11 @@
 
 public class BirinciGun {
 
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("11:57 birinci gun");
+		System.out.println("ikinci commit");
 	}
 
 }
