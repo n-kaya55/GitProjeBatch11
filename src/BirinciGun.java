beb7d77ef55a7169e5a5ef813c0f7a071d9e5909
n-kaya55 <74555873+n-kaya55@users.noreply.github.com>
2020-11-17 22:26:39 +0300

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -6,6 +6,7 @@ public class BirinciGun {
 	public static void main(String[] args) {
 		System.out.println("11:57 birinci gun");
 		System.out.println("ikinci commit");
+		System.out.println("ikinci gun");
 	}
 
 }
